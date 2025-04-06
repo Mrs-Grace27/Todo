dfac88cd0bf0fea108e1947701ebcd127c0bab85
Mrs-Grace27 <gracejohanson9@gmail.com>
2025-04-06 13:00:46 +0300

    Delete todo directory

--- todo/backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('tasks/', views.create_task, name='create_task'),
-    path('manage_task/<int:task_id>/', views.manage_task, name='manage_task'),
-]
