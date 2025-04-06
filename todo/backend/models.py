86f217c463136c2a8125a355c42d3a00e9ec20aa
Mrs-Grace27 <gracejohanson9@gmail.com>
2025-04-06 13:03:45 +0300

    Add files via upload

---
+++ todo/backend/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    content = models.TextField()
+    completed = models.BooleanField(default=False)
+    deadline = models.DateTimeField()
+    description = models.TextField()
+    status = models.CharField(max_length=50, default="In Progress")
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Task: {self.title[:30]}"
